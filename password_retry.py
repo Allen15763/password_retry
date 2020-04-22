2fbefce3ee0fe670de214e9e3ceecfba2d2ba5c5
Allen Li <love22798800@gmail.com>
2020-04-22 23:18:18 +0800

    no endless loop

--- password_retry.py
+++ password_retry.py
@@ -1,7 +1,7 @@
 
 password = 'a123456'
 chance = 3
-while True:
+while chance > 0:
 	text = input('Please type your password: ')
 	if text == password:
 		print('log in success!')
@@ -9,9 +9,7 @@ while True:
 	else:
 		chance = chance -1
 		print('wrong password,', chance, 'times left')
-		if chance == 0:
-			print('bye')		
-			break
+
 
 
 
