d30b68e0403d2faef19ca42d128d822960f07b28
Allen Li <love22798800@gmail.com>
2020-04-22 23:25:33 +0800

    optimization

--- password_retry.py
+++ password_retry.py
@@ -2,13 +2,18 @@
 password = 'a123456'
 chance = 3
 while chance > 0:
+	chance = chance -1  #每一次輸入密碼都消耗一次
 	text = input('Please type your password: ')
 	if text == password:
 		print('log in success!')
 		break
 	else:
-		chance = chance -1
-		print('wrong password,', chance, 'times left')
+		print('wrong password,')
+		if chance > 0:
+			print(chance, 'times left')
+		else:
+			print('Please contact customer service')
+
 
 
 
